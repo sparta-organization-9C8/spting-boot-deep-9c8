c902f2486704fe45d11e5d74d509a7fcf8211ca8
jeongsoo <dnjawm19@naver.com>
2022-10-17 16:03:15 +0900

    test

--- login-live-session2-main/src/main/java/com/example/loginlivesession2/account/controller/AccountController.java
+++ login-live-session2-main/src/main/java/com/example/loginlivesession2/account/controller/AccountController.java
@@ -39,5 +39,5 @@ public class AccountController {
         response.addHeader(JwtUtil.ACCESS_TOKEN, jwtUtil.createToken(userDetails.getAccount().getEmail(), "Access"));
         return new GlobalResDto("Success IssuedToken", HttpStatus.OK.value());
     }
-
+asd
 }
