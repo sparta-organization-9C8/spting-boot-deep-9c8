6ef1241b322230d92e196c73b1e9aefc15361b96
jeongsoo <dnjawm19@naver.com>
2022-10-18 17:40:10 +0900

    like controller 추가

--- login-live-session2-main/src/main/java/com/example/loginlivesession2/comment/entity/Comment.java
+++ login-live-session2-main/src/main/java/com/example/loginlivesession2/comment/entity/Comment.java
@@ -1,4 +1,39 @@
 package com.example.loginlivesession2.comment.entity;
 
-public class Comment {
+import com.example.loginlivesession2.Timestamped;
+import com.example.loginlivesession2.comment.dto.CommentDto;
+import com.example.loginlivesession2.post.entity.Post;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class Comment extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(nullable = true)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    @JsonIgnore
+    private Post post;
+
+    public Comment(CommentDto requestDto, Post post_get) {
+        this.content = requestDto.getContent();
+        this.post = post_get;
+    }
+
+    public void update(CommentDto requestDto) {
+        this.content = requestDto.getContent();
+    }
+
 }
--- login-live-session2-main/src/main/java/com/example/loginlivesession2/like/controller/LikeController.java
+++ login-live-session2-main/src/main/java/com/example/loginlivesession2/like/controller/LikeController.java
@@ -1,4 +1,38 @@
 package com.example.loginlivesession2.like.controller;
 
+import com.example.loginlivesession2.account.entity.Account;
+import com.example.loginlivesession2.jwt.util.JwtUtil;
+import com.example.loginlivesession2.security.user.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/like")
 public class LikeController {
+    private final LikesService likesService;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/{postId}")
+    public Account createLike(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Account account = userDetails.getAccount();
+//        String email = jwtUtil.getEmailFromToken(token);
+        return account;
+//        return likesService.createLike(postId, email);
+    }
+
+    // 좋아요 저장로직
+    // 좋아요를 누른 경우 (좋아요 해제, 좋아요 추가)
+    // front에서 받아와야 하는 정보 (postid, memberId)
+
+    // 유저가 해당 게시글에 좋아요를 누른 여부 확인
+    // Boolean bool = likeRepository.existsBy{User}And{Post}(userId, postId)
+//    likesRepository.findByUserAndPost(user, post)
+
+    // bool이 true면 좋아요를 누른 상태 -> 삭제 해주면 됩니다.
+    // bool이 false면 좋아요를 안 누른 상태 -> 저장 해주면 됩니다.
 }
